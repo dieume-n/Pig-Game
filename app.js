80337b35082eef809087aa2113e508aefe56f9d2
Luteranya Nyange <dieume.dev@gmail.com>
2019-01-18 10:52:10 +0300

    display the rolled number in the current result box

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-var playersScores, roundScore, activePlayer, dice;
+var playersScores, roundScore, activePlayer;
 
 playerScores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
-dice = Math.floor(Math.random() * 6) + 1;
+activePlayer = 0;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    // Random Number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    // Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'img/dice-' + dice + '.png';
+
+    //Update the round score if the rolled number is not 1
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    } else {
+        // Next player
+    }
+});
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
