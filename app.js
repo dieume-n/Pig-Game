7d91ef1d601fe57da8ded92e3a35611036006295
Luteranya Nyange <dieume.dev@gmail.com>
2019-01-19 18:56:13 +0300

    simple type-checking

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     // Reset roundScore to 0
     roundScore = 0;
-    document.getElementById('current-0').textContent = 0;
-    document.getElementById('current-1').textContent = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
 
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
